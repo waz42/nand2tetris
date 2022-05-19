04f9467ead18a8dbd73c91f63a069fabcf1f1ca7
awazu <awazu42@gmail.com>
2022-05-19 20:07:46 +0900

    complete project04

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,26 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+    @R2
+    M=0     // R2 = 0
+    @i
+    M=1     // i = 1
+(LOOP)
+    @i
+    D=M     // D = i
+    @R1     
+    D=D-M   // D = i - R1
+    @END
+    D;JGT   // if D > 0 then goto END
+    @R0
+    D=M     // D = R0
+    @R2
+    M=D+M   // R2 += R0
+    @i
+    M=M+1   // i++
+    @LOOP
+    0;JMP   // Goto LOOP
+(END)
+    @END
+    0;JMP   // Goto END (infinite loop)
